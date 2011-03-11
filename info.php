ea493b900cac9728a31182396f8d9d78f849f009
hertsch <ralf.hertsch@phpmanufaktur.de>
2011-03-11 18:38:44 +0000

    git-svn-id: https://server3.subversion-server.com/phpmanufaktur/svn/dbWatchSite/trunk@12 cd3ca8ef-0760-244b-8890-41fee552339e

--- info.php
+++ info.php
@@ -5,7 +5,7 @@
  * 
  * @author Ralf Hertsch (ralf.hertsch@phpmanufaktur.de)
  * @link http://phpmanufaktur.de
- * @copyright 2010
+ * @copyright 2010 - 2011
  * @license GNU GPL (http://www.gnu.org/licenses/gpl.html)
  * @version $Id$
  * 
